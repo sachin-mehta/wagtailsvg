77b75cfc709be8afa58e078c015971c7904676ac
Sachin Mehta <smehta7@schools.nyc.gov>
2025-07-17 09:48:35 +0000

    wagtail 7 upgrade compatibility WIP

--- wagtailsvg/edit_handlers.py
+++ wagtailsvg/edit_handlers.py
@@ -1,20 +1,9 @@
-try:
-    from wagtail.admin.panels import FieldPanel
-    from wagtailsvg.widgets import AdminSvgChooser
+from wagtail.admin.panels import FieldPanel
+from wagtailsvg.widgets import AdminSvgChooser
 
-    class SvgChooserPanel(FieldPanel):
-        def __init__(self, field_name, disable_comments=None, permission=None, **kwargs):
-            super().__init__(field_name, **kwargs)
-            self.widget = AdminSvgChooser
-            self.disable_comments = disable_comments
-            self.permission = permission
-
-except ImportError:
-    from wagtail.admin.panels import BaseChooserPanel
-
-    class SvgChooserPanel(BaseChooserPanel):
-        object_type_name = "svg"
-
-        def widget_overrides(self):
-            from wagtailsvg.widgets import AdminSvgChooser
-            return {self.field_name: AdminSvgChooser}
+class SvgChooserPanel(FieldPanel):
+    def __init__(self, field_name, disable_comments=None, permission=None, **kwargs):
+        super().__init__(field_name, **kwargs)
+        self.widget = AdminSvgChooser
+        self.disable_comments = disable_comments
+        self.permission = permission
--- wagtailsvg/widgets.py
+++ wagtailsvg/widgets.py
@@ -1,5 +1,4 @@
 from wagtailsvg.models import Svg
-from wagtail import VERSION as WAGTAIL_VERSION
 from django.template.loader import render_to_string
 from django.utils.translation import gettext_lazy as _
 from generic_chooser.widgets import AdminChooser
@@ -10,59 +9,62 @@ class AdminSvgChooser(AdminChooser):
     choose_one_text = _("Choose an SVG")
     choose_another_text = _("Choose another SVG")
     link_to_chosen_text = _("Edit this SVG")
-    model = Svg
-    choose_modal_url_name = 'svg_chooser:choose'
-    edit_item_url_name = 'wagtailsvg_svg_modeladmin_edit'
     clear_choice_text = _("Clear choice")
-    template = 'wagtailsvg/widgets/chooser.html'
+
+    model = Svg
+    choose_modal_url_name = "svg_chooser:choose"
+    edit_item_url_name = "wagtailsvg_svg_modeladmin_edit"
+    template = "wagtailsvg/widgets/chooser.html"
 
     def get_value_data(self, value):
+        instance = None
+
         if value is None:
-            instance = None
-        elif self.model and isinstance(value, self.model):
+            pass
+        elif isinstance(value, self.model):
             instance = value
             value = value.pk
         else:
             try:
                 instance = self.get_instance(value)
-            except (ObjectDoesNotExist if self.model is None else self.model.DoesNotExist):
-                instance = None
+            except (ObjectDoesNotExist, self.model.DoesNotExist):
+                pass
+
         if instance is None:
             return {
-                'value': None,
-                'title': '',
-                'edit_item_url': None,
-                'preview_url': None,
+                "value": None,
+                "title": "",
+                "edit_item_url": None,
+                "preview_url": None,
             }
-        else:
-            return {
-                'value': value,
-                'title': self.get_title(instance),
-                'edit_item_url': self.get_edit_item_url(instance),
-                'preview_url': instance.url,
-            }
+
+        return {
+            "value": value,
+            "title": self.get_title(instance),
+            "edit_item_url": self.get_edit_item_url(instance),
+            "preview_url": getattr(instance, "url", None),
+        }
 
     def render_html(self, name, value, attrs):
-        if WAGTAIL_VERSION >= (2, 12):
-            value_data = value
-        else:
-            value_data = self.get_value_data(value)
+        value_data = self.get_value_data(value)
+        original_field_html = self.render_input_html(name, value_data["value"], attrs)
 
-        original_field_html = self.render_input_html(name, value_data['value'], attrs)
-        return render_to_string(self.template, {
-            'widget': self,
-            'original_field_html': original_field_html,
-            'attrs': attrs,
-            'is_empty': value_data['value'] is None,
-            'title': value_data['title'],
-            'edit_item_url': value_data['edit_item_url'],
-            'create_item_url': self.get_create_item_url(),
-            'choose_modal_url': self.get_choose_modal_url(),
-            'preview_url': value_data['preview_url']
-        })
+        context = {
+            "widget": self,
+            "original_field_html": original_field_html,
+            "attrs": attrs,
+            "is_empty": value_data["value"] is None,
+            "title": value_data["title"],
+            "edit_item_url": value_data["edit_item_url"],
+            "create_item_url": self.get_create_item_url(),
+            "choose_modal_url": self.get_choose_modal_url(),
+            "preview_url": value_data["preview_url"],
+        }
+
+        return render_to_string(self.template, context)
 
     class Media:
         js = [
-            'generic_chooser/js/chooser-modal.js',
-            'wagtailsvg/js/chooser-widget.js',
+            "generic_chooser/js/chooser-modal.js",
+            "wagtailsvg/js/chooser-widget.js",
         ]
