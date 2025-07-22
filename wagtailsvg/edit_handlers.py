8d213ccd5ea898d606b4790c2b81c93c0338b742
Sachin Mehta <smehta7@schools.nyc.gov>
2025-07-22 08:44:32 +0000

    initialized AdminSvgChooser in SVGChooserPanel

--- wagtailsvg/edit_handlers.py
+++ wagtailsvg/edit_handlers.py
@@ -3,7 +3,6 @@ from wagtailsvg.widgets import AdminSvgChooser
 
 class SvgChooserPanel(FieldPanel):
     def __init__(self, field_name, disable_comments=None, permission=None, **kwargs):
-        super().__init__(field_name, **kwargs)
-        self.widget = AdminSvgChooser
+        super().__init__(field_name, widget=AdminSvgChooser(), **kwargs)
         self.disable_comments = disable_comments
         self.permission = permission
