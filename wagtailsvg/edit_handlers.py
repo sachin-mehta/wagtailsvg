9bc936bea1c01782da1c1f2f54496c6a50e78bcb
Sachin Mehta <smehta7@schools.nyc.gov>
2025-07-22 08:51:52 +0000

    Remove widget from kwargs

--- wagtailsvg/edit_handlers.py
+++ wagtailsvg/edit_handlers.py
@@ -3,6 +3,7 @@ from wagtailsvg.widgets import AdminSvgChooser
 
 class SvgChooserPanel(FieldPanel):
     def __init__(self, field_name, disable_comments=None, permission=None, **kwargs):
+        kwargs.pop("widget", None)
         super().__init__(field_name, widget=AdminSvgChooser(), **kwargs)
         self.disable_comments = disable_comments
         self.permission = permission
